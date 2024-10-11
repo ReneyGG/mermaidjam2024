80d3d3f9b7a9d11a71a8e39212f775dc6aef56d2
Bartłomiej Kasprzyk <dolarbartek@wp.pl>
2024-10-11 23:01:15 +0200

    hit flash

--- scenes/base/base.gd
+++ scenes/base/base.gd
@@ -10,5 +10,7 @@ func _process(delta):
 func take_damage(damage):
 	health = max(0, health - damage)
 	if health == 0:
-		#print("BASE DESTROYED")
 		queue_free()
+	$Sprite2D.material.set_shader_parameter("active",true)
+	await get_tree().create_timer(.1).timeout
+	$Sprite2D.material.set_shader_parameter("active",false)
--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -25,6 +25,9 @@ func take_damage(damage):
 	health = max(0, health - damage)
 	if health == 0:
 		queue_free()
+	$Sprite2D.material.set_shader_parameter("active",true)
+	await get_tree().create_timer(.1).timeout
+	$Sprite2D.material.set_shader_parameter("active",false)
 
 func attack():
 	#print("attack")

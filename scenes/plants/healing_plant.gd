7be023c104e26228b21734d8e0dd022697556b76
Bartłomiej Kasprzyk <dolarbartek@wp.pl>
2024-10-12 20:32:49 +0200

    things

--- scenes/plants/healing_plant.gd
+++ scenes/plants/healing_plant.gd
@@ -5,4 +5,11 @@ extends "res://scenes/plant_base/plant_base.gd"
 func _on_heal_timer_timeout():
 	for plant in healing_area.get_overlapping_areas():
 		if plant.is_in_group("plant") and plant != self:
+			if plant.health == plant.current_health:
+				return
 			plant.take_heal(1)
+			#var heal_line = Line2D.new()
+			#add_sibling(heal_line)
+			#heal_line.set_points([Vector2(global_position), plant.global_position])
+			#heal_line.texture = load("res://icon.svg")
+			
